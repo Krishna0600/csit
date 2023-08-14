ead2a60e1ed44c82eac176129942ce989ce1e84b
Krishna0600 <138752726+Krishna0600@users.noreply.github.com>
2023-08-14 11:01:32 +0545

    Add files via upload

---
+++ firstprogram.js
@@ -0,0 +1,15 @@
+function printHello(){
+    document.getElementById("demo").innerHTML ="Hello";
+    window.alert('Hello There');
+    console.log('error line');
+    
+}
+
+
+function printHello1(){
+   
+    let a=5000;
+    let b=0.15;
+    let c=a*b;
+    document.getElementById("demo1").innerHTML =c;
+}
